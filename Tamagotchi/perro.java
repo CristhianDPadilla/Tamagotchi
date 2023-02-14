cd91eaa9dfdb240f38d97873f3d57f30163ede56
Ancizarc <124370636+Ancizarc@users.noreply.github.com>
2023-02-13 21:20:42 -0500

    2 atributos y 2 métodos de la clase perro

--- Tamagotchi/perro.java
+++ Tamagotchi/perro.java
@@ -1,6 +1,30 @@
 
 public class perro {
     //Atributos
-
+    
+    private String nombre;
+    private String raza;
+    
+    
+    
+    
+    
+    
+    
     //Métodos
+    
+    public void ladrar(){
+        
+        System.out.println("Guau Guau");
+    
+    }
+    
+    public void aullar(){
+    
+        System.out.println("aúúúúúú");
+    }
+    
+        
+    
+    
 }
