8c671fefd8e8a991dd29dac027a982af4dc227d1
CristhianDPadilla <cristhiand.padilla221@umariana.edu.co>
2023-02-13 22:25:21 -0500

    Constructor, metodos y atributos

--- Tamagotchi/perro.java
+++ Tamagotchi/perro.java
@@ -4,11 +4,19 @@ public class perro {
     
     String nombre;
     String raza;
+    String genero;
+    int    edad;
     
     
     
     
-    
+    //Método constructor
+    public perro (String nombre, String raza, String genero, int edad){
+        this.nombre = nombre;
+        this.raza = raza;
+        this.genero = genero;
+        this.edad = edad;
+    }
     
     
     //Métodos
@@ -22,6 +30,16 @@ public class perro {
     public void aullar(){
     
         System.out.println("aúúúúúú");
+    }
+
+    public void comer(){
+
+        System.out.println("ñam ñam ñam");
+    }
+
+    public void Acostarse(){
+
+        System.out.println("Zzzzzzzz");
     }
     
         
