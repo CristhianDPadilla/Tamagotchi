23383d80082141c0d75f4ad8ed199f23b68d79bf
ingejohan1813 <johans.ordonez221@umariana.edu.co>
2023-02-13 20:54:20 -0500

    Agregando metodos

--- Tamagotchi/gato.java
+++ Tamagotchi/gato.java
@@ -2,7 +2,20 @@ public class gato {
     //Atributos 
     String nombre;
     int edad;
+
+    //Metodo constructor
+    public gato (String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
     
+    //Metodos
+    public void dormir(){
+        System.out.println("Zzzzzzz");
+    }
+
+    public void maullar(){
+        System.out.println("Miau Miau");
     }
     
 }
